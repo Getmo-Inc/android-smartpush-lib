eb409d82e4d158ed3f60ead366e450ff5b130d0f
DBServer - Fabio Rodrigo Licks <t.licks@admm001050.zaffari.com.br>
2017-08-11 17:27:11 -0300

    update

--- gcm-smartpush-lib/src/main/java/br/com/smartpush/SmartpushListenerService.java
+++ gcm-smartpush-lib/src/main/java/br/com/smartpush/SmartpushListenerService.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 
 import com.google.android.gms.gcm.GcmListenerService;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * Created by fabio.licks on 10/02/16.
  */
@@ -20,6 +23,37 @@ public abstract class SmartpushListenerService extends GcmListenerService {
     // [START receive_message]
     @Override
     public void onMessageReceived( String from, Bundle data ) {
+        String[] types = { "BANNER", "SLIDER", "CARROUSSEL", "SUBSCRIBE_EMAIL", "SUBSCRIBE_PHONE" };
+
+        // Do something cool here...
+        if ( data != null ) {
+            if ( data.containsKey( "type" ) ) {
+                String pushType = data.getString( "type" );
+                if ( Arrays.asList( types ).contains( pushType ) ) {
+                    // fetch payload extra & put it on bundle...
+//                    POST /notifications/extra
+
+//                  Params
+                    HashMap<String, String> params = new HashMap<>();
+
+//                  devid: (required) string | "000000000000000"
+                    String devId =
+                            Utils.Smartpush.getMetadata( this, Utils.Constants.SMARTP_API_KEY );
+                    params.put( "devid", devId );
+
+//                  appid: (required) string | "000000000000000"
+                    String appId =
+                            Utils.Smartpush.getMetadata( this, Utils.Constants.SMARTP_APP_ID );
+                    params.put( "appid", appId );
+
+//                  pushid: (required) string | "00000000000000000000000000000000"
+                    String pushId = SmartpushHitUtils
+                            .getValueFromPayload( SmartpushHitUtils.Fields.PUSH_ID, data );
+                    params.put( SmartpushHitUtils.Fields.PUSH_ID.getParamName(), pushId );
+
+                }
+            }
+        }
 
         new SmartpushNotificationManager( this ).onMessageReceived( from, data );
 
